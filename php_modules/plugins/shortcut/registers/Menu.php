80062c75ceee7c22d1d7bf18a874ee1364422d20
tinh chu <cvtinh2000@gmail.com>
2023-11-24 12:37:45 +0700

    update render menu

--- php_modules/plugins/shortcut/registers/Menu.php
+++ php_modules/plugins/shortcut/registers/Menu.php
@@ -1,25 +1,73 @@
 <?php
-// namespace App\plugins\shortcut\registers;
+namespace App\plugins\shortcut\registers;
 
-// use SPT\Application\IApp;
-// use SPT\Support\Loader;
+use SPT\Application\IApp;
+use SPT\Support\Loader;
 
-// class Menu
-// {
-//     public static function registerItem( IApp $app )
-//     {
-//         $container = $app->getContainer();
-//         $router = $container->get('router');
-//         $ShortcutModel = $container->get('ShortcutModel');
+class Menu
+{
+    public static function registerItem( IApp $app )
+    {
+        $container = $app->getContainer();
+        $router = $container->get('router');
+        $path_current = $router->get('actualPath');
+        $ShortcutModel = $container->get('ShortcutModel');
 
-//         $shortcuts = $ShortcutModel->getShortcut();
-//         foreach()
-//         {
+        $shortcuts = $ShortcutModel->getShortcut();
+        $menu = [];
+        foreach($shortcuts as $shortcut)
+        {
+            if (isset($shortcut['childs']) && $shortcut['childs'])
+            {
+                $parent_menu = [];
+                if ($shortcut['group'])
+                {
+                    $parent_menu =  [
+                            'link' => '', 
+                            'title' => $shortcut['group'], 
+                            'icon' => '<i class="fa-solid fa-link"></i>',
+                            'class' => '',
+                            'childs' => [],
+                        ];
+                }
 
-//         }
-//         return [
-//             // 'menu'=> $menu,
-//             // 'order' => 2,
-//         ];
-//     }
-// }
+                $active_group = false;
+                $child_menu = [];
+                foreach($shortcut['childs'] as $item)
+                {
+                    if(trim($path_current, '/') == trim(str_replace($router->url(), '' ,$item['link']), '/'))
+                    {
+                        $active = 'active';
+                        $active_group = true;
+                    }
+                    else
+                    {
+                        $active = '';
+                    }
+
+                    $child_menu[] = [
+                        'link' => $item['link'], 
+                        'title' => $item['name'], 
+                        'icon' => '<i class="fa-solid fa-link"></i>',
+                        'class' => $active,
+                    ];
+                }
+
+                if ($parent_menu)
+                {
+                    $parent_menu['childs'] = $child_menu;
+                    $menu[] = $parent_menu;
+                }
+                else
+                {
+                    $menu = array_merge($menu, $child_menu);
+                }
+            }
+        }
+       
+        return [
+            'menu'=> $menu,
+            'order' => 1,
+        ];
+    }
+}
