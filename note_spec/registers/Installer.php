1086551624b08bdc8a817cda4c3c9692473f7330
tinh chu <cvtinh2000@gmail.com>
2023-12-15 14:40:49 +0700

    create note install

---
+++ note_spec/registers/Installer.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\plugins\pnote\note_spec\registers;
+
+use SPT\Application\IApp;
+use App\plugins\pnote\note_spec\entities\TreeNoteEntity;
+
+class Installer
+{
+    public static function info()
+    {
+        return ['tags'=>['pnote']];
+    }
+    
+    public static function name()
+    {
+        return 'Plugin note spec';
+    }
+
+    public static function detail()
+    {
+        return [
+            'author' => 'Pham Minh',
+            'created_at' => '2023-01-03',
+            'description' => 'Plugin note spec'
+        ];
+    }
+
+    public static function version()
+    {
+        return '0.0.1';
+    }
+
+    public static function install( IApp $app)
+    {
+        // DB Entity
+        $container = $app->getContainer();
+        $TreeNoteEntity = new TreeNoteEntity($container->get('query'));
+        $TreeNoteEntity->checkAvailability();
+    }
+    public static function uninstall( IApp $app)
+    {
+        // run sth to uninstall
+    }
+    public static function active( IApp $app)
+    {
+        // run sth to prepare the install
+    }
+    public static function deactive( IApp $app)
+    {
+        // run sth to uninstall
+    }
+}
---
+++ note_upload/registers/Installer.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\plugins\pnote\note_upload\registers;
+
+use SPT\Application\IApp;
+use App\plugins\pnote\note_upload\entities\FileEntity;
+
+class Installer
+{
+    public static function info()
+    {
+        return ['tags'=>['pnote']];
+    }
+    
+    public static function name()
+    {
+        return 'Plugin note upload';
+    }
+
+    public static function detail()
+    {
+        return [
+            'author' => 'Pham Minh',
+            'created_at' => '2023-01-03',
+            'description' => 'Plugin note upload'
+        ];
+    }
+
+    public static function version()
+    {
+        return '0.0.1';
+    }
+
+    public static function install( IApp $app)
+    {
+        // DB Entity
+        $container = $app->getContainer();
+        $FileEntity = new FileEntity($container->get('query'));
+        $TreeNoteEntity->checkAvailability();
+    }
+    public static function uninstall( IApp $app)
+    {
+        // run sth to uninstall
+    }
+    public static function active( IApp $app)
+    {
+        // run sth to prepare the install
+    }
+    public static function deactive( IApp $app)
+    {
+        // run sth to uninstall
+    }
+}
