093e7c8b97dc4a216eed2c6e25e1b0b5fbd42dd8
joomaio.huylq <dauxotcachua99@gmail.com>
2024-02-05 17:15:58 +0700

    add logic create super user

--- pnote/registers/Installer.php
+++ pnote/registers/Installer.php
@@ -67,8 +67,95 @@ class Installer
         
     }
 
-    public static function createSuperUser()
+    public static function createSuperUser(IApp $app)
     {
+        $container = $app->getContainer();
+        $super_user_groups = [];
+            $user_groups = $container->get('GroupEntity')->list(0, 0, []);
+            foreach ($user_groups as $group) {
+                if (str_contains($group['access'], 'user_manager')) {
+                    $super_user_groups[] = $group['id'];
+                }
+            }
+
+            if (count($super_user_groups) == 0) {
+                $access = $container->get('PermissionModel')->getAccess();
+
+                // Create group
+                $group = [
+                    'name' => 'Super',
+                    'description' => 'Super Group',
+                    'access' => json_encode($access),
+                    'status' => 1,
+                    'created_by' => 0,
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'modified_by' => 0,
+                    'modified_at' => date('Y-m-d H:i:s')
+                ];
+
+                $created_group = $container->get('GroupEntity')->add($group);
+
+                if (!$created_group) {
+                    echo 'Create Group Failed';
+                    return false;
+                }
+            }
+
+            $super_users = $container->get('UserGroupEntity')->list(0, 0, ['group_id IN (' . implode(',', $super_user_groups) . ')']);
+
+            if (count($super_user_groups) == 0 || count($super_users) == 0) {
+                echo "Plugin Pnote requires to create super admin. \n";
+                $enter_info = true;
+                while ($enter_info) {
+                    $name = readline("Enter your name:\n");
+                    $username = readline("Enter your username:\n");
+                    $email = readline("Enter your email:\n");
+                    $password = readline("Enter your password (At least 6 characters):\n");
+
+                    $user = [
+                        'username' => $username,
+                        'name' => $name,
+                        'email' => $email,
+                        'status' => 1,
+                        'password' => $password,
+                        'confirm_password' => $password,
+                        'created_by' => 0,
+                        'created_at' => date('Y-m-d H:i:s'),
+                        'modified_by' => 0,
+                        'modified_at' => date('Y-m-d H:i:s')
+                    ];
+
+                    $validate = $container->get('UserEntity')->validate($user);
+                    if (!$validate) {
+                        echo "Information you entered is invalid. Please enter again! \n";
+                    } else {
+                        $enter_info = false;
+                    }
+                }
+
+                $created_user = $container->get('UserEntity')->add($user);
+                if (!$created_user) {
+                    echo 'Create User Failed';
+                    return false;
+                }
+
+                if (count($super_user_groups) == 0) {
+                    $group_id = $created_group;
+                } else {
+                    $group_id = $super_user_groups[0];
+                }
+
+                $created_user_group = $container->get('UserGroupEntity')->add([
+                    'group_id' => $group_id,
+                    'user_id' => $created_user,
+                ]);
+
+                if (!$created_user_group) {
+                    echo  'Create User Group Failed';
+                    return false;
+                }
+
+            }
         return true;
     }
 
